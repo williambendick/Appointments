85cfd1fef84ddc4f1a99dd20fd04ad924bf10a80
Bill <williambendick@gmail.com>
2021-02-24 12:49:26 -0500

    added UI page for testing zoom api

---
+++ Appointments/Zoom/TestUI.aspx
@@ -0,0 +1,24 @@
+﻿
+<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="TestUI.aspx.vb" Inherits="Appointments.TestUI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Zoom Test</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="margin: auto; width: 90%;">
+        <br /><br />
+        <asp:Button ID="btnCreateGoogleEvent" runat="server" Text="Create Google Event" CssClass="btn btn-primary btn-lg" Width="700px" style="max-width:700px;" Font-Size="12pt" Height="50px"/>
+        <br /><br />
+        <asp:Button ID="btnCreateZoomMeeting" runat="server" Text="Create Zoom Meeting" CssClass="btn btn-primary btn-lg" Width="700px" style="max-width:700px;" Font-Size="12pt" Height="50px"/>
+        <br /><br />
+        <asp:Label ID="Label1" runat="server" Font-Size="15pt" Text="Result: " Font-Bold="True"></asp:Label>
+        <asp:Label ID="lblResult" runat="server" Font-Size="15pt" Font-Names="Arial"></asp:Label>
+        <asp:HiddenField ID="hdnCalendarEventId" runat="server" ></asp:HiddenField>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Appointments/Zoom/TestUI.aspx.vb
@@ -0,0 +1,64 @@
+﻿Public Class TestUI
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+    End Sub
+    Protected Sub btnCreateGoogleEvent_Click(sender As Object, e As EventArgs) Handles btnCreateGoogleEvent.Click
+
+        Dim evt As GoogleEvent = New GoogleEvent()
+
+        If hdnCalendarEventId.Value = String.Empty Then
+            evt.Summary = "Test creation of event"
+            evt.Location = "a location"
+            evt.StartTime = Date.Now.AddMinutes(60)
+            evt.EndTime = Date.Now.AddMinutes(120)
+            evt.Create()
+        Else
+            evt.Summary = "Test updating of event"
+            evt.Update(hdnCalendarEventId.Value)
+        End If
+
+        If evt.Result.Error = Nothing Then
+            If hdnCalendarEventId.Value = String.Empty Then
+                lblResult.Text = "Google event created with id " & evt.Result.Id
+                btnCreateGoogleEvent.Text = "Update summary for Google event with id " & evt.Result.Id
+                hdnCalendarEventId.Value = evt.Result.Id
+            Else
+                hdnCalendarEventId.Value = String.Empty
+                lblResult.Text = "Google event with id " & evt.Result.Id & " was updated."
+                btnCreateGoogleEvent.Text = "Create Google Event"
+            End If
+        Else
+            hdnCalendarEventId.Value = String.Empty
+            lblResult.Text = "An error occurred: " & evt.Result.Error
+        End If
+
+    End Sub
+
+    Protected Sub CreateZoomMeeting_Click(sender As Object, e As EventArgs) Handles btnCreateZoomMeeting.Click
+
+        'if zoom refresh token does not exist for this user, send to zoom to authorize application
+        If Request.Cookies("ZAT") Is Nothing Then
+            Zoom.API.RequestUserAuthorization("create-meeting")
+        Else
+            Dim meeting As Zoom.Meeting = New Zoom.Meeting() With
+            {
+                .Topic = "Test Meeting",
+                .StartTime = Date.Now.AddHours(1),
+                .Duration = 120
+            }
+
+            Dim result As String = meeting.Create()
+
+            If result = "success" Then
+                lblResult.Text = "Zoom meeting created with id " & meeting.Id.ToString()
+            ElseIf result = "access token does not exist" Then
+                Zoom.API.RequestUserAuthorization("create-meeting")
+            Else
+                lblResult.Text = "An error occurred: " & result
+            End If
+        End If
+
+    End Sub
+
+End Class
