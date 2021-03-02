5268c512fa510c205aa22b01b2d2d9c591a310fb
Bill <williambendick@gmail.com>
2021-03-02 03:17:09 -0500

    meeting class modifications:
    - made properties nullable so that their default vals are null and they do not get included in api requests unless a val is set for them
    - added ShouldSerializeId function so that Id property is included when deserializing api responses, but not when serializing api requests
    - added update and delete functions
    - refactored MakeCreationRequest function so that it can be used for update and delete requests

--- Appointments/Zoom/Models/Meeting.vb
+++ Appointments/Zoom/Models/Meeting.vb
@@ -1,26 +1,34 @@
-﻿
+﻿Imports Newtonsoft.Json
 Namespace Zoom
     Public Class Meeting
 
-        Public Property Id As Long
+        Public Property Id As Long?
 
         Public Property Topic As String
 
-        Public Property StartTime As Date
+        Public Property StartTime As Date?
 
-        Public Property Duration As Integer
+        Public Property Duration As Integer?
+
+        Private ReadOnly Property BaseUrl As String = "https://api.zoom.us/v2"
+
+        Public Function ShouldSerializeId() As Boolean
+            Return False
+        End Function
 
         Public Function Create() As String
 
+            Dim endpoint As String = "/users/me/meetings"
+
             Try
-                Dim result As Response(Of Meeting) = MakeCreationRequest()
+                Dim result As Response(Of Meeting) = MakeRequest(endpoint, "POST", Me)
 
                 'if there is an error it may be because the access token has expired, so get a new token and try request again
                 If result.Error IsNot Nothing Then
 
                     Dim refreshedAccessToken As String = API.RefreshToken()
 
-                    result = MakeCreationRequest(refreshedAccessToken)
+                    result = MakeRequest(endpoint, "post", Me, refreshedAccessToken)
 
                     If result.Error IsNot Nothing Then Return result.Error.Message
 
@@ -35,19 +43,72 @@ Namespace Zoom
 
         End Function
 
-        Private Function MakeCreationRequest(Optional refreshedToken As String = Nothing) As Response(Of Meeting)
+        Public Function Update() As String
+
+            If Id = 0 Then Return "meeting id is required"
+
+            Dim endpoint As String = "/meetings/" & Id
+
+            Try
+                Dim result As Response(Of Meeting) = MakeRequest(endpoint, "PATCH", Me)
+
+                'if there is an error it may be because the access token has expired, so get a new token and try request again
+                If result.Error IsNot Nothing Then
+
+                    Dim refreshedAccessToken As String = API.RefreshToken()
+
+                    result = MakeRequest(endpoint, "patch", Me, refreshedAccessToken)
+
+                    If result.Error IsNot Nothing Then Return result.Error.Message
+
+                End If
+
+                Return "success"
+
+            Catch ex As Exception
+                Return ex.Message
+            End Try
+
+        End Function
+
+        Public Function Delete() As String
+
+            If Id = 0 Then Return "meeting id is required"
+
+            Dim endpoint As String = "/meetings/" & Id
+
+            Try
+                Dim result As Response(Of Meeting) = MakeRequest(endpoint, "DELETE")
+
+                'if there is an error it may be because the access token has expired, so get a new token and try request again
+                If result.Error IsNot Nothing Then
+
+                    Dim refreshedAccessToken As String = API.RefreshToken()
+
+                    result = MakeRequest(endpoint, "delete", Nothing, refreshedAccessToken)
+
+                    If result.Error IsNot Nothing Then Return result.Error.Message
+
+                End If
+
+                Return "success"
+
+            Catch ex As Exception
+                Return ex.Message
+            End Try
+        End Function
+
+        Private Function MakeRequest(endpoint As String, httpMethod As String, Optional meetingData As Meeting = Nothing, Optional refreshedToken As String = Nothing) As Response(Of Meeting)
 
             Dim accessToken As String = If(refreshedToken, API.GetToken("access"))
 
             If accessToken Is Nothing Then Throw New Exception("access token does not exist")
 
-            Dim requestUri As String = "https://api.zoom.us/v2/users/me/meetings"
-
             Dim requestHeaders As NameValueCollection = New NameValueCollection() From {
                 {"Authorization", "Bearer " & accessToken}
             }
 
-            Return API.Request(Of Meeting, Meeting)(requestUri, "post", requestHeaders, Me)
+            Return API.Request(Of Meeting, Meeting)(BaseUrl & endpoint, httpMethod, requestHeaders, meetingData)
 
         End Function
     End Class
