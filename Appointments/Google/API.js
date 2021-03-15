5407a9090a2582058e9a2c087695c840f5be291f
Bill <williambendick@gmail.com>
2021-03-15 17:20:28 -0400

    added configurations for managing calendars with Google JS library

---
+++ Appointments/Google/API.js
@@ -0,0 +1,153 @@
+﻿
+var CLIENT_ID = '';
+var API_KEY = '';
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+var SCOPES = "https://www.googleapis.com/auth/calendar";
+var GoogleAuth;
+
+async function makeCalendarAPIRequest(requestType, requestData) {
+    
+    var result;
+
+    // load gapi library if it has not been loaded yet
+    if (!GoogleAuth) {
+
+        await gapi.load('client:auth2');
+
+        result = await initializeGAPI();
+
+        if (result != 'success') return formatErrorMessage('initializeGAPI', result);
+    }
+
+    // sign in user if they're not signed in
+    if (!GoogleAuth.isSignedIn.get()) {
+
+        result = await signInUser();
+
+        if (result != 'success') return formatErrorMessage('signInUser', result);
+    }
+
+    // make API request
+    switch (requestType) {
+
+        case 'create':
+            return await createEvent(requestData);
+
+        case 'update':            
+            return await updateEvent(requestData);
+
+        case 'delete':
+            return await deleteEvent(requestData);
+
+        default:
+            return 'a valid type was not included with request data.';
+    }
+}
+
+function initializeGAPI() {
+    return new Promise(function (resolve) {
+        gapi.client.init({
+            apiKey: API_KEY,
+            clientId: CLIENT_ID,
+            discoveryDocs: DISCOVERY_DOCS,
+            scope: SCOPES
+        }).then(function () {
+            GoogleAuth = gapi.auth2.getAuthInstance();
+
+            resolve('success');
+           
+        }, function (error) {
+            resolve(error);
+        });
+    });
+}
+
+function signInUser() {
+    return new Promise(function (resolve) {
+        GoogleAuth.signIn()
+            .then(function () {
+                resolve('success');
+            }, function (error) {
+                resolve(error);
+            });
+    });
+}
+
+function makeRequest(apiRequest) {
+    return new Promise(resolve => {
+        apiRequest.execute(response => resolve(response));
+    });
+}
+
+async function createEvent(event) {
+
+    var request = gapi.client.calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event
+    });
+
+    var result = await makeRequest(request);
+
+    if (result.id)
+        return 'Event created with id ' + result.id;
+    else
+        return formatErrorMessage('makeRequest', result);  
+}
+
+async function updateEvent(event) {
+
+    var request = gapi.client.calendar.events.patch({
+        'calendarId': 'primary',
+        'eventId': event.id,
+        'resource': event
+    });
+
+    var result = await makeRequest(request);
+
+    if (result.error)
+        return formatErrorMessage('makeRequest', result);  
+    else
+        return 'Event with id ' + result.id + ' was updated'
+}
+
+async function deleteEvent(event) {
+
+    var request = gapi.client.calendar.events.delete({
+        'calendarId': 'primary',
+        'eventId': event.id,
+        'resource': event
+    });
+
+    var result = await makeRequest(request);
+
+    if (result.error)
+        return formatErrorMessage('makeRequest', result);
+    else
+        return 'Event with id ' + event.id + ' was deleted'
+}
+
+function formatErrorMessage(errorType, errorData) {
+
+    var message = 'An error occurred';
+
+    if (errorType === 'initializeGAPI') {
+        message += ' when initializing Google API library';
+    }
+    else if (errorType === 'signInUser') {
+        message += ' when signing in user';
+    } 
+    else if (errorType === 'makeRequest') {
+        message += ' when making API request';
+    } 
+
+    if (errorData) {
+
+        if (errorData.error && typeof errorData.error === 'string') message += ': ' + errorData.error;
+
+        if (errorData.details) message += ': ' + errorData.details;
+
+        if (errorData.error && errorData.error.message) message += ': ' + errorData.error.message;
+    }
+
+    return message;
+}
---
+++ Appointments/Google/UI.aspx
@@ -0,0 +1,86 @@
+﻿
+<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="UI.aspx.vb" Inherits="Appointments.UI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Google Test</title>
+    <style>.ui-button {max-width:700px; outline: 0 !important;}</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="margin: auto; width: 90%;">
+        <br /><br />
+
+        <asp:Button ID="btnCreateMeeting" runat="server" Text="Create Meeting" CssClass="ui-button btn btn-primary btn-lg" 
+            Width="300px" Font-Size="12pt" Height="45px" BorderWidth="0px" UseSubmitBehavior="False" OnClientClick="createMeeting(); return false;" />
+        <br /><br />        
+
+        <asp:Button ID="btnUpdateMeeting" runat="server" Text="Update Meeting" CssClass="ui-button btn btn-primary btn-lg" 
+            Width="300px" Font-Size="12pt" Height="45px" BorderWidth="0px" UseSubmitBehavior="False" OnClientClick="updateMeeting(); return false;" />
+        &nbsp
+        <asp:TextBox ID="txtUpdateMeeting" runat="server" Height="35px" Width="250px" placeholder="Meeting ID" BorderColor="#F0F0F0" BorderStyle="Solid" style="padding: 0px 15px;"></asp:TextBox>
+        <br /><br />
+
+        <asp:Button ID="btnDeleteMeeting" runat="server" Text="Delete Meeting" CssClass="ui-button btn btn-primary btn-lg" 
+            Width="300px" Font-Size="12pt" Height="45px" BorderWidth="0px" UseSubmitBehavior="False" OnClientClick="deleteMeeting(); return false;" />
+        &nbsp
+        <asp:TextBox ID="txtDeleteMeeting" runat="server" Height="35px" Width="250px" placeholder="Meeting ID" BorderColor="#F0F0F0" BorderStyle="Solid" style="padding: 0px 15px;"></asp:TextBox>
+        <br /><br /><br />
+
+        <asp:Label ID="Label1" runat="server" Font-Size="12pt" Font-Names="Arial" Font-Bold="True" Text="Result: "></asp:Label>
+        <span id="lblResult" style="font-family: Arial; font-size: 11pt"></span>
+    </div>
+    </form>
+
+    <script src="https://apis.google.com/js/client.js"></script>
+    <script src="luxon.min.js"></script>
+    <script src="API.js"></script>
+
+    <script>
+
+        var _txtUpdateMeeting = document.getElementById('<%=txtUpdateMeeting.ClientID %>')
+        var _txtDeleteMeeting = document.getElementById('<%=txtDeleteMeeting.ClientID %>')
+        var _lblResult = document.getElementById('lblResult');
+       
+        async function createMeeting() {
+
+            // note: using luxon 'T' instead of 'HH:mm:ss' causes 12 AM times to be incorrectly formated with hour as 24 instead of 0 
+            var start = luxon.DateTime.now().plus({ hours: 1, }).toFormat("yyyy-LL-dd'T'HH:mm:ssZZ");
+            var end = luxon.DateTime.now().plus({ hours: 2, }).toFormat("yyyy-LL-dd'T'HH:mm:ssZZ");
+
+            var eventData = {
+                'summary': 'test summary',
+                'location': 'test location',
+                'start': { 'dateTime': start },
+                'end': { 'dateTime': end }
+            };
+
+            var result = await makeCalendarAPIRequest('create', eventData);
+            _lblResult.innerText = result;
+        }
+
+        async function updateMeeting() {
+
+            var eventData = {
+                'id': _txtUpdateMeeting.value,
+                'summary': 'updated summary'
+            };
+
+            var result = await makeCalendarAPIRequest('update', eventData);
+            _lblResult.innerText = result;
+        }
+
+        async function deleteMeeting() {
+
+            var eventData = {'id': _txtDeleteMeeting.value};
+
+            var result = await makeCalendarAPIRequest('delete', eventData);
+            _lblResult.innerText = result;
+        }
+
+    </script>
+  
+</body>
+</html>
