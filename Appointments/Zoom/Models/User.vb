84fccd48236e2e5d6c28d1420386cc6b17cc0235
Bill <williambendick@gmail.com>
2021-03-23 14:53:35 -0400

    added user class and configured for retrieving zoom user

---
+++ Appointments/Zoom/Models/User.vb
@@ -0,0 +1,38 @@
+﻿
+Namespace Zoom
+    Public Class User
+
+        Public Property Id As String
+
+        Public Property FirstName As String
+
+        Public Property LastName As String
+
+        Public Property Email As String
+
+        Public Function Retrieve(accessToken As String) As String
+
+            Try
+                Dim requestHeaders As NameValueCollection = New NameValueCollection() From {
+                    {"Authorization", "Bearer " & accessToken}
+                }
+
+                Dim result As Response(Of User) = API.Request(Of User, Object)(API.BaseUrl & "/users/me", "GET", requestHeaders, Nothing)
+
+                If result.Error IsNot Nothing Then Return result.Error.Message
+
+                Id = result.Data.Id
+                FirstName = result.Data.FirstName
+                LastName = result.Data.LastName
+                Email = result.Data.Email
+
+                Return "success"
+
+            Catch ex As Exception
+                Return ex.Message
+            End Try
+
+        End Function
+
+    End Class
+End Namespace
