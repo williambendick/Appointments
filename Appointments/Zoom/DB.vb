f70f70ad2ab49f3ac353271c1e2931e8df9f9336
Bill <williambendick@gmail.com>
2021-03-26 06:02:30 -0400

    added database class for testing in dev environment

---
+++ Appointments/Zoom/DB.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DB
+
+    Public Shared Function GetData(sql As String) As String
+
+        Try
+
+            Dim cs As String = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=bmt;Integrated Security=True;Connect Timeout=30;Encrypt=False;" &
+                "TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+
+            Using cn As New SqlConnection(cs)
+
+                Try
+                    Dim cmd As SqlCommand = New SqlCommand(sql, cn)
+                    cn.Open()
+
+                    Dim result = cmd.ExecuteScalar()
+
+                    Return If(IsDBNull(result), Nothing, result)
+
+                Catch ex As Exception
+                    Dim s As String = ex.Message
+                End Try
+            End Using
+
+        Catch ex As Exception
+            Return ex.Message
+        End Try
+
+    End Function
+
+
+
+    Public Shared Function Update(sql As String) As String
+
+        Try
+
+            Dim cs As String = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=bmt;Integrated Security=True;Connect Timeout=30;Encrypt=False;" &
+                "TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+
+            Dim result As Integer
+
+            Using cn As New SqlConnection(cs)
+
+                Try
+                    Dim cmd As SqlCommand = New SqlCommand(sql, cn)
+                    cn.Open()
+
+                    result = cmd.ExecuteNonQuery()
+
+                Catch ex As Exception
+                    Dim s As String = ex.Message
+                End Try
+            End Using
+
+        Catch ex As Exception
+            Return ex.Message
+        End Try
+
+        Return ""
+
+    End Function
+
+End Class
